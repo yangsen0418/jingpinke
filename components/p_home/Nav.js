717e98c61a6c224a4246ebdfabf354f0ed5d5cee
yangsen0418 <952208207@qq.com>
2022-06-29 09:06:18 +0800

    首页开发

--- components/p_home/Nav.js
+++ components/p_home/Nav.js
@@ -1,3 +1,35 @@
-export default function Nav() {
-    return <section>Nav</section>
+import Link from 'next/link'
+import ReactSlick from 'react-slick'
+import s from './Nav.module.css'
+
+const Nav = ({ data = [] }) => {
+    const settings = {
+        dots: true,
+        rows: 2,
+        infinite: false,
+        speed: 100,
+        slidesToShow: 4,
+        slidesToScroll: 3,
+        initialSlide: 0,
+    };
+
+    return (
+        <div className={s.nav}>
+            <section className={s.wrap}>
+                <ReactSlick {...settings}>
+                    {data.map((item, index) => (
+                        <div key={`${index}-${item.id}`}>
+                            {/* 跳转链接 */}
+                            <Link href="/course/detail/[id]" as={`course/detail/${item.id}`}>
+                                {/* Nav图片 */}
+                                <a className={s.fon}> <img src={item.img} key={index} alt={item.title} className={s.slide} />{item.title}</a>
+                            </Link>
+                        </div>
+                    ))}
+                </ReactSlick>
+            </section>
+        </div>
+    )
 }
+
+export default Nav
